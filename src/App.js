790452da9a4b5bbd07a61607b19d92d2a577a4dc
dav770 <47830985+dav770@users.noreply.github.com>
2020-09-04 17:10:08 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,19 +3,24 @@ import GAListener from 'components/GAListener';
 import { EmptyLayout, LayoutRoute, MainLayout } from 'components/Layout';
 import PageSpinner from 'components/PageSpinner';
 import AuthPage from 'pages/AuthPage';
+import Planning from 'pages/Planning';
 import React from 'react';
 import componentQueries from 'react-component-queries';
-import { BrowserRouter, Redirect, Route, Switch, NavLink } from 'react-router-dom';
+import {
+  BrowserRouter,
+  Redirect,
+  Route,
+  Switch,
+  NavLink,
+} from 'react-router-dom';
 import './styles/reduction.scss';
 
-import aes256 from 'aes256'
-
-
+import aes256 from 'aes256';
 
 import Profile from './components/Profile';
 import TestCp from './components/TestCp';
 
-import {isEmpty} from "lodash"
+import { isEmpty } from 'lodash';
 import ListeAgents from './pages/ListeAgents';
 import ListeCommerciaux from './pages/ListeCommerciaux';
 import Clients from './pages/Clients';
@@ -43,133 +48,125 @@ const getBasename = () => {
   // return `/${process.env.PUBLIC_URL.split('/').pop()}`;
 };
 
-
-
-
-
 class App extends React.Component {
-constructor(props) {
-  super(props)
+  constructor(props) {
+    super(props);
 
-  this.state = {
-  user:'',
+    this.state = {
+      user: '',
       email: 'demo@gmail.com',
-      pwd:aes256.encrypt(this.keyAes, 'demoPwd770@'),
+      pwd: aes256.encrypt(this.keyAes, 'demoPwd770@'),
       rememberMe: true,
-      validate: aes256.encrypt(this.keyAes,'date of day'),
+      validate: aes256.encrypt(this.keyAes, 'date of day'),
       auth: 1, //0=a refaire, 1=true, 2=false
       origine: '/',
-      level:0,
-      actif:true,
-      confirmPwd:'',
-      comment:'',
+      level: 0,
+      actif: true,
+      confirmPwd: '',
+      comment: '',
+    };
   }
 
-
- 
-}
-
-
-
-callHeader = (logOut)=>{
-  
-  if(logOut){
-    // si demande logout force render avec appel de la fonction verifredirect pour revenir sur login et interdir nav vers dashboard
-    // puisque value validate du localstorage est supprimee depuis header.js
-    this.setState({auth: 0, origine:'/'})
-  }
-}
-
-keyAes = 'key of secure aes256'
-
-
-componentDidMount() {
-  console.log(' monte');
-  
-  
-  this.verifRedirect()
-}
-
-
-
-
-verifRedirect = ()=>{
-  console.log('rentre dans verif', this.state)
-
-  this.auth = 0 //ouverture de l 'appli doit etre proteger pour eviter navigation depuis url sans etre authentifie
-
-  this.getStorage = localStorage.getItem('userActif')
-  this.validateStorage = ''
- 
- 
-   if (this.getStorage === null || isEmpty(this.getStorage)){
-    const storage = {
-      user: this.state.user,
-      email: this.state.email,
-      pwd:this.state.pwd,
-      rememberMe: this.state.rememberMe,
-      validate: aes256.encrypt(this.keyAes, this.state.validate)
+  callHeader = logOut => {
+    if (logOut) {
+      // si demande logout force render avec appel de la fonction verifredirect pour revenir sur login et interdire nav vers dashboard
+      // puisque value validate du localstorage est supprimee depuis header.js
+      this.setState({ auth: 0, origine: '/' });
     }
-     this.getStorage = localStorage.setItem('userActif', JSON.stringify(storage))
-     this.validateStorage = JSON.parse(this.getStorage)
-     
-   }else{
-     this.validateStorage = JSON.parse(this.getStorage)
-   }
+  };
 
-   console.log('comparaison verif',aes256.decrypt(this.keyAes, this.validateStorage.validate) === this.dateConnect.getDate().toString()+this.dateConnect.getMonth().toString()+this.dateConnect.getFullYear().toString())
-   if( aes256.decrypt(this.keyAes, this.validateStorage.validate) === this.dateConnect.getDate().toString()+this.dateConnect.getMonth().toString()+this.dateConnect.getFullYear().toString()){
-     this.setState({user:this.validateStorage.user, email:this.validateStorage.email, level:0 , auth: 1, origine:''})
+  keyAes = 'key of secure aes256';
 
-     
-   }else{
-    this.setState({auth: 2},()=>{console.log('sort de verif', this.state)})
+  componentDidMount() {
+    console.log(' monte');
+
+    this.verifRedirect();
+  }
+
+  verifRedirect = () => {
     
-   }
+    
+    this.auth = 0; //ouverture de l 'appli doit etre protegee pour eviter navigation depuis url sans etre authentifie
 
-   
+    this.getStorage = localStorage.getItem('userActif');
+    this.validateStorage = '';
+console.log('verif redirect APP ',this.getStorage);
 
-}
+    if (this.getStorage === null || isEmpty(this.getStorage)) {
+      const storage = {
+        user: this.state.user,
+        email: this.state.email,
+        pwd: this.state.pwd,
+        rememberMe: this.state.rememberMe,
+        validate: aes256.encrypt(this.keyAes, this.state.validate),
+      };
+      localStorage.setItem('userActif',JSON.stringify(storage));
+      this.validateStorage = storage;
+      console.log('rentre dans verif', this.validateStorage);
+      
+    } else {
+      this.validateStorage = JSON.parse(this.getStorage);
+    }
 
+    // console.log(
+    //   'comparaison verif',
+    //   aes256.decrypt(this.keyAes, this.validateStorage.validate) ===
+    //     this.dateConnect.getDate().toString() +
+    //       this.dateConnect.getMonth().toString() +
+    //       this.dateConnect.getFullYear().toString(),
+    // );
+    if (
+      aes256.decrypt(this.keyAes, this.validateStorage.validate) ===
+      this.dateConnect.getDate().toString() +
+        this.dateConnect.getMonth().toString() +
+        this.dateConnect.getFullYear().toString()
+    ) {
+      this.setState({
+        user: this.validateStorage.user,
+        email: this.validateStorage.email,
+        level: 0,
+        auth: 1,
+        origine: '',
+      });
+    } else {
+      this.setState({ auth: 2 }, () => {
+        console.log('sort de verif', this.state);
+      });
+    }
+  };
 
+  recieveLogin=(auth)=>{
+auth && this.setState({auth: 1})
 
-dateConnect = new Date()
+  }
 
+  componentDidMount() {
+    console.log('render didmount');
+    // this.verifRedirect()
+  }
+  
 
-
-
-
+  dateConnect = new Date();
 
   render() {
-    
-    
     // origine === '/' lancement de l'appli
     
-// remttre
-
-// if (this.state.origine === '/' && this.state.auth === 0){
-//   console.log('lancement de  l app', this.state)
-//   this.verifRedirect()
-// }
-
-
-
+    // if (this.state.origine === '/' && this.state.auth === 0){
+    //   console.log('render APP',this.state.origine, "state APP:", this.state);
+    //   this.verifRedirect()
+    // }
+    
+    
     return (
       <BrowserRouter basename={getBasename()}>
-
-
-
-      
         <GAListener>
           <Switch>
-
-
-<LayoutRoute
+            <LayoutRoute
               exact
               path="/login"
               layout={EmptyLayout}
               component={props => (
-                <AuthPage {...props} authState={STATE_LOGIN} />
+                <AuthPage {...props} authState={STATE_LOGIN} callBackLogin={this.recieveLogin}/>
               )}
             />
             <LayoutRoute
@@ -181,24 +178,43 @@ dateConnect = new Date()
               )}
             />
 
-{/* remttre */}
-{/* {(this.state.auth === 2 ) && <Redirect to='/login'/>} */}
+            {console.log("valeur auth",this.state.auth, this.state.origine )}
+            {/* {(this.state.auth === 2 ) &&  <Redirect to='/login'/>} 
+            {(this.state.auth === 1 ) && <Redirect to='/'/>} */}
+            {/*  */}
 
-
-
-            <MainLayout breakpoint={this.props.breakpoint} recieveMainHeader={this.callHeader} returnStorage={this.state} >
+            <MainLayout
+              breakpoint={this.props.breakpoint}
+              recieveMainHeader={this.callHeader}
+              returnStorage={this.state}
+            >
               <React.Suspense fallback={<PageSpinner />}>
+              {console.log("valeur route",)}
 
                 <Route exact path="/" component={DashboardPage} />
-                <Route path="/profile" component={()=>(<Profile userOpen={this.state}/>)}></Route>
+                <Route
+                  path="/profile"
+                  component={() => <Profile userOpen={this.state} />}
+                ></Route>
                 {/* <Route path="/agents" component={()=>(<Agents userOpen={this.state}/>)}></Route> */}
-                <Route path="/agents" component={()=>(<ListeAgents userOpen={this.state}/>)}></Route>
-                <Route path="/commerciaux" component={()=>(<ListeCommerciaux userOpen={this.state}/>)}></Route>
-                <Route path="/clients" component={()=>(<Clients userOpen={this.state}/>)}></Route>
-                
+                <Route
+                  path="/agents"
+                  component={() => <ListeAgents userOpen={this.state} />}
+                ></Route>
+                <Route
+                  path="/commerciaux"
+                  component={() => <ListeCommerciaux userOpen={this.state} />}
+                ></Route>
+                <Route
+                  path="/clients"
+                  component={() => <Clients userOpen={this.state} />}
+                ></Route>
+                <Route
+                  path="/planning"
+                  component={() => <Planning userOpen={this.state} />}
+                ></Route>
               </React.Suspense>
             </MainLayout>
-            
           </Switch>
         </GAListener>
       </BrowserRouter>
